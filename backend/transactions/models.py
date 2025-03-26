d660696e482204f9f54150ed74389fb852df7363
Seungmin Heo <nolzaheo@gmail.com>
2025-03-26 23:38:35 +0900

    transactions 테이블 생성용 models.py

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,3 +1,18 @@
+# transactions/models.py
 from django.db import models
+from accounts.models import Account
+import uuid
 
-# Create your models here.
+class Transaction(models.Model):
+    transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender_account = models.ForeignKey(Account, related_name='sent_transactions', on_delete=models.CASCADE)
+    receiver_account = models.ForeignKey(Account, related_name='received_transactions', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=18, decimal_places=2)
+    status = models.CharField(max_length=50, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'transactions'  # 테이블 이름을 'transactions'로 지정
+
+    def __str__(self):
+        return str(self.transaction_id)
