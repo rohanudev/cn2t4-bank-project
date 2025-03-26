17dcdd3a38d35217c6913b8d0d038819e8c936af
Seungmin Heo <nolzaheo@gmail.com>
2025-03-26 23:36:21 +0900

    users 테이블 생성용 models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
+# users/models.py
 from django.db import models
+import uuid
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    status = models.CharField(max_length=50, default='ACTIVE')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'  # 테이블 이름을 'users'로 지정
+
+    def __str__(self):
+        return self.name
