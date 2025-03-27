8f864110cb7522eb1fa2bea564a42fbb3eb7f796
Seungmin Heo <nolzaheo@gmail.com>
2025-03-27 10:17:30 +0900

    users 테이블 생성용 models.py 업데이트

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,13 +1,18 @@
-# users/models.py
-from django.db import models
 import uuid
+from django.db import models
 
 class User(models.Model):
+    STATUS_CHOICES = [
+        ('ACTIVE', 'Active'),
+        ('LOCKED', 'Locked'),
+        ('DELETED', 'Deleted'),
+    ]
+
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
-    status = models.CharField(max_length=50, default='ACTIVE')
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ACTIVE')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,4 +20,4 @@ class User(models.Model):
         db_table = 'users'  # 테이블 이름을 'users'로 지정
 
     def __str__(self):
-        return self.name
+        return self.email
