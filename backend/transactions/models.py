a10eac1d8d0a3172e2e3de51e0143c95551d0eb4
Seungmin Heo <nolzaheo@gmail.com>
2025-03-27 10:18:46 +0900

    transactions 테이블 생성용 models.py 업데이트

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -1,18 +1,31 @@
-# transactions/models.py
+import uuid
 from django.db import models
 from accounts.models import Account
-import uuid
+
 
 class Transaction(models.Model):
-    transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    sender_account = models.ForeignKey(Account, related_name='sent_transactions', on_delete=models.CASCADE)
-    receiver_account = models.ForeignKey(Account, related_name='received_transactions', on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=18, decimal_places=2)
-    status = models.CharField(max_length=50, default='PENDING')
-    created_at = models.DateTimeField(auto_now_add=True)
+    TRANSACTION_CHOICES = [
+        ('DEPOSIT', 'Deposit'),
+        ('WITHDRAWAL', 'Withdrawal'),
+        ('TRANSFER', 'Transfer')
+    ]
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('SUCCESS', 'Success'),
+        ('FAILED', 'Failed')
+    ]
 
+    transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sent_transactions', null=True, blank=True)
+    receiver_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='received_transactions', null=True, blank=True)
+    amount = models.IntegerField()
+    type = models.CharField(max_length=12, choices=TRANSACTION_CHOICES)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='PENDING')
+    message = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
-        db_table = 'transactions'  # 테이블 이름을 'transactions'로 지정
+        db_table = 'transactions'
 
     def __str__(self):
-        return str(self.transaction_id)
+        return f'{self.type} : {self.amount} (from {self.sender_account} to {self.receiver_account})'
