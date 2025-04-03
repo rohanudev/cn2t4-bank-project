bd6d90995ffe104b6a7e961a5dc8bf70b9251af8
seungmin <nolzaheo@gmail.com>
2025-04-03 11:41:23 +0900

    Add userinfo.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,6 @@
 import { goTo } from "./router.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  goTo("landing", {});
+  //goTo("userInfo", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
 });
 
