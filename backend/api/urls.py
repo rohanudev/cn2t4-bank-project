d2f4268d0ba94fc2c6489df8bc01b8ba4abf7781
seungmin <nolzaheo@gmail.com>
2025-04-04 01:31:44 +0900

    accounts 모델 수정 및 마이그레이션 파일 추가

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('hello', views.hello_view),
     path('users/', include('users.urls')),
+    path('accounts/', include('accounts.urls')),
     path('transactions/', include('transactions.urls')),
 ]
