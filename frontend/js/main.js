df23444652f2fd160f62e42dee59c5b8117d7d46
hyunwoo <hyunwoo@hyunwooui-MacBookPro.local>
2025-04-07 16:44:52 +0900

    fixture 충돌 해결

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   //goTo("landing", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
   //goTo("userInfoEdit", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
 
+
   //goTo("deposit", {accountId});
   //goTo("withdraw", {accountId});
   //goTo("transfer", {accountId});
   //goTo("account", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
+
 });
 
 
