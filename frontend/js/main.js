6ae2344b2c9763a99be5c480b775b82dbdbd148e
hyunwoo <hyunwoo@hyunwooui-MacBookPro.local>
2025-04-07 17:19:59 +0900

    초기 입금액 제거

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,9 @@
 import { goTo } from "./router.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // goTo("login");
+  goTo("login");
   //goTo("userInfo", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
-  goTo("landing", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
+  //goTo("landing", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
   //goTo("userInfoEdit", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
 
   //goTo("deposit", {accountId});
