59016cc69799d587249f0f7e60d767671b93a9ad
seungmin <nolzaheo@gmail.com>
2025-04-08 22:36:08 +0900

    CORS 도메인 추가

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
+    "https://tikklemoa.com",
     "http://localhost",
 ]
 
--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://api.tikklemoa.com";
