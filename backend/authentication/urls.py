f16250a063820c8ba078005dce83bc0bee8992ca
Bro-o <62311763+Bro-o@users.noreply.github.com>
2025-04-09 17:48:57 +0900

    에러 수정
    - f 스트링 뺌
    - views 이름 빠진거 추가

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login', views.login, name='login'),
-    path("api/token/refresh/", refresh_token_view),
+    path("api/token/refresh/", views.refresh_token_view, name="refresh_token"),
     path('api/refresh', views.refresh_token, name='refresh_token'),
     path('api/logout', views.logout, name='logout'),
 ]
