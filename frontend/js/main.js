5f1898df87a442e401d17a8c6d530948af51c7ac
seungmin <nolzaheo@gmail.com>
2025-04-09 13:32:31 +0900

    accountDelete.js 삭제

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,14 +2,6 @@ import { goTo } from "./router.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   goTo("login");
-  //goTo("userInfo", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
-  //oTo("landing", { userId: "b4e8cd6c10117010456033172212c2f1" });
-  //goTo("userInfoEdit", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
-  //goTo("accountDetail", { userId: "b4e8cd6c10117010456033172212c2f1" });
-  //goTo("deposit", {accountId});
-  //goTo("withdraw", {accountId});
-  //goTo("transfer", {accountId});
-  //goTo("account", { userId: "bf7dfc9e-6e59-46e8-9ef4-efaabb2fe51b" });
 });
 
 
