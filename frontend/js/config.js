3ad7e7ec99f812fc38f3a7363906d7fe10935d79
seungmin <nolzaheo@gmail.com>
2025-04-11 16:51:52 +0900

    url 변경

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,7 @@
 // js/config.js
-//export const API_BASE_URL = "https://api.tikklemoa.com";
-export const API_BASE_URL = "http://localhost:8000";
+
+/*url for distribution*/
+export const API_BASE_URL = "https://api.tikklemoa.com";
+
+/*url for local test*/
+//export const API_BASE_URL = "http://localhost:8000";
