f7fd979ec6c22f4694cacdfdaf38c5f1c1d1a5c5
Bro-o <62311763+Bro-o@users.noreply.github.com>
2025-04-11 17:46:27 +0900

    health check용 / 응답 추가

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import include, path
 
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
+    path('', health_check),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
