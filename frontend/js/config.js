8cd56b6cd62cd95363619f519c4d78efcd7a3923
강준우 <junwoo@gangjun-uui-MacBookPro.local>
2025-04-28 20:41:43 +0900

    config: settings.py 분리

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 
 /*url for distribution*/
-export const API_BASE_URL = "https://api.tikklemoa.com";
+// export const API_BASE_URL = "https://api.tikklemoa.com";
 
 /*url for local test*/
-// export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "http://localhost:8000";
