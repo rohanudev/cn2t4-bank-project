46d7b33f16d9c0684e25d09a51954ecc305d7ae9
강준우 <junwoo@gangjun-uui-MacBookPro.local>
2025-04-29 14:08:18 +0900

    config: 환경변수 설정에 따른 로컬, 배포 환경 변경

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -107,20 +107,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv("DB_ENGINE"),
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-        'OPTIONS': {
-            'charset': os.getenv("DB_CHARSET"),
-            'init_command': os.getenv("DB_INIT_COMMAND"),
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv("DB_ENGINE"),
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USER"),
+#         'PASSWORD': os.getenv("DB_PASSWORD"),
+#         'HOST': os.getenv("DB_HOST"),
+#         'PORT': os.getenv("DB_PORT"),
+#         'OPTIONS': {
+#             'charset': os.getenv("DB_CHARSET"),
+#             'init_command': os.getenv("DB_INIT_COMMAND"),
+#         },
+#     }
+# }
 
 DATABASES = {
     'default': {
